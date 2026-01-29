940b20d1f7f850f31d525fdda2c27f3880ff0405
JoffreDavid <jdariasb@uce.edu.ec>
2026-01-28 23:37:04 -0500

    Commit inicial

---
+++ src/clients/MatriculaClient.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const consultarTodos = ()=> {
+    const data = axios.get('http://localhost:8081/matricula/api/v1.0/estudiantes').then(r=>r.data);
+    return data;
+}
+
+export async function consultarTodosFacade(){
+    return await consultarTodos();
+}
