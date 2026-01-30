a918d88a47c427683d72cccb2ce2daa61b89b71e
JoffreDavid <jdariasb@uce.edu.ec>
2026-01-30 00:35:14 -0500

    Taller 33 integración frontend con backend

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <AboutComponent/>
 </template>
+
+<script>
+import AboutComponent from "../components/AboutComponent.vue"
+export default {
+  components: {
+    AboutComponent,
+
+  },
+}
+</script>
+
+<style>
+
+</style>
