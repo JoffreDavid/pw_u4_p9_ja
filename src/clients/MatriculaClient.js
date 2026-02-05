f3b37451838a6886174ca983ffd6a12515bfc2fd
JoffreDavid <jdariasb@uce.edu.ec>
2026-02-05 13:28:34 -0500

    Taaller 35: autentiacion 2.0

--- src/clients/MatriculaClient.js
+++ src/clients/MatriculaClient.js
@@ -1,34 +1,37 @@
 import axios from "axios";
 
+const TOKEN= "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJtYXRyaWN1bGEtYXV0aCIsInN1YiI6Inhpbm5ldGgiLCJncm91cHMiOlsiYWRtaW4iXSwiaWF0IjoxNzcwMzE0MDQ4LCJleHAiOjE3NzAzMTc2NDgsImp0aSI6IjJlMDFjZDljLTM5YTUtNGFjMi05NzgzLTIzMWE1Nzk4OWM0MiJ9.Oz3RWBymfm3EpzL7ouYcZFLjbIko84wVQ-kZwzJr_hqdjvWVgV0DE3KdmaswOiQClWCGKfUC40ZhJRssdoHRrFAABQEG6fIFpmKhYYPXj_7txUR65aGwBNFYY3FrDjqD-N1cuKnR5KaJBLFZSd-ogNE0crSf1cQYDTAHY49M__v1pihj-kE_5nLbn_wzVszIxSlXYFc-8JzkZTQQLocade_7Net5ZiIEfO3I-0ot_TPyrPXN9FfdX23OOvh4lmbazSxsqHGuObV2V3jB2JFw9edfgfc2K3GExT9b-456zgmHDl8ygsfOAw4_wVypQsvZD2N5qcu4cMkhpVL7sxbrVw";
+const URL= "http://localhost:8081/matricula/api/v1.0/estudiantes";
+
 const consultarTodos = async ()=> {
-    const data = axios.get('http://localhost:8081/matricula/api/v1.0/estudiantes').then(r=>r.data);
+    const data = axios.get(`${URL}`,{headers:{Authorization: `Bearer ${TOKEN}`}}).then(r=>r.data);
     return data;
 }
 
 
 const consultarPorId = async (id) => {
-    const data = axios.get(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`).then(r=>r.data)
+    const data = axios.get(`${URL}/${id}`,{headers:{Authorization: `Bearer ${TOKEN}`}}).then(r=>r.data)
     return data;
 }
 
 const guardar = async (body) => {
-   
-    axios.post(`http://localhost:8081/matricula/api/v1.0/estudiantes`,body).then(r=>r.data);
+   const token = 
+    axios.post(`${URL}`,body, {headers:{Authorization: `Bearer ${TOKEN}`}}).then(r=>r.data);
     
 } 
 
 const actualizar = async (id, body) =>{
-    axios.put(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`,body).then(r=>r.data);
+    axios.put(`${URL}/${id}/${id}`,body, {headers:{Authorization: `Bearer ${TOKEN}`}}).then(r=>r.data);
    
 }
 
 const actualizarParcial = async (id, body) =>{
-    axios.patch(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`,body).then(r=>r.data);
+    axios.patch(`${URL}/${id}/${id}`,body, {headers:{Authorization: `Bearer ${TOKEN}`}}).then(r=>r.data);
   
 }
 
 const borrar = async (id) => {
-    axios.delete(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`).then(r=>r.data);
+    axios.delete(`${URL}/${id}`).then(r=>r.data);
 }
 
 
